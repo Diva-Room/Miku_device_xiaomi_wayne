87c9988729652e2bfdeda2055891752c6923e7b9
TH779 <tongsj2333@gmail.com>
2020-09-05 22:24:11 +0800

    sdm660-common: Import XiaomiParts from sdm845-common
     * From https://github.com/LineageOS/android_device_xiaomi_sdm845-common/commit/65f0dd1a73e2088ceeca6922adef90fde21420e9.
    
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Change-Id: Idf661a96f770a70725c38e13edcc28ab379af2b6
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -320,6 +320,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi_sdm660-libperfmgr \
