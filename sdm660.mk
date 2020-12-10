dfb04d84e2e3e9b6ae3ea2f3ee44665a29ea87cc
sbwml <984419930@qq.com>
2020-12-10 13:49:03 +0800

    sdm660-common: bump vendor.display.config to 2.0
    * New WFD blobs need vendor.display.confi@2.0.so

--- sdm660.mk
+++ sdm660.mk
@@ -191,8 +191,8 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.9 \
-    vendor.display.config@1.9_vendor
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0_vendor
 
 # Doze
 PRODUCT_PACKAGES += \
