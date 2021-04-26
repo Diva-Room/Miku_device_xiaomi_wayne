17261fedb893ba9b11753410950616d5f30bec36
Ratoriku <a1063021545@gmail.com>
2021-04-26 20:18:55 +0800

    sdm660-common: Drop unused perf-profile.conf
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -325,9 +325,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi_sdm660-libperfmgr
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
-
 # Protobuf
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
