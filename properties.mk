3f947671cb2f3fd1515805cf0291b3d952e4a887
Ratoriku <a1063021545@gmail.com>
2021-07-13 18:36:09 +0800

    sdm660-common: Commonize color management prop
    * To enable color mode in setting/display
    
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- properties.mk
+++ properties.mk
@@ -123,6 +123,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.use_color_management=true \
     ro.vendor.display.cabl=0 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
