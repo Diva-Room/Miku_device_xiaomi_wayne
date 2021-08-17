3096c2f8061f9c1cd3a72aac84fecdc7b2e320c8
kubersharma001 <kubersharma001@gmail.com>
2021-08-17 17:37:41 +0000

    sdm660-common: Compile libldacBT_bco.so
    08-17 12:10:55.180 19022 19075 E a2dp_vendor_ldac_decoder: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found
    
    Change-Id: I910382a3affe8d02d6b0b20fd2dc3e91e57f3e9a

--- sdm660.mk
+++ sdm660.mk
@@ -124,6 +124,7 @@ PRODUCT_BOOT_JARS += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
+    libldacBT_bco \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
