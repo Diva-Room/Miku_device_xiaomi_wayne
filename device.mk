58ba83a185dce4a4220dc9f7ee9a8501a9c9c1a7
Edwin Moquete <edwinmmoquete@gmail.com>
2021-09-17 18:23:59 -0400

    wayne: Build QTI thermal HAL
    Change-Id: I9c7b96945e0d417a4620b2a8d4cbddd64eb9d9b7
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- device.mk
+++ device.mk
@@ -398,6 +398,9 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.wayne.qti
+
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
     
