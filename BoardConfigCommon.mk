c50a481bea8ac6631093c0ca23978d1ae9a8c6ce
Ratoriku <a1063021545@gmail.com>
2021-10-06 18:01:15 +0800

    [DNM]sdm660-common: Set BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES to true
    Change-Id: Iabaecbab85f932719f7edd93ad5033ab684eaf76

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,6 +6,8 @@
 
 COMMON_PATH := device/xiaomi/sdm660-common
 
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm660
 TARGET_NO_BOOTLOADER := true
