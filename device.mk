cceb891e1cc648eeb52e123d312dd520a22420d5
Edwin Moquete <edwinmmoquete@gmail.com>
2021-11-14 13:26:49 -0400

    wayne: Bring back stock thermal config
    Minimal configs from stock for charging and lcd control,
    adapted for kernel 4.19

--- device.mk
+++ device.mk
@@ -399,7 +399,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+    $(DEVICE_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
     
 # Trust
 PRODUCT_PACKAGES += \
