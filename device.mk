a65fa6a0e627ccb562a11e775dd4c92d4bc47640
xiaoleGun <1592501605@qq.com>
2022-10-01 09:10:27 +0800

    wayne: Update Graphics blobs from S62Pro
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -166,6 +166,7 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
     libdisplayconfig \
+    libgralloc.qti \
     libtinyxml \
     libtinyxml.vendor \
     libqdMetaData \
@@ -182,7 +183,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.1
+    vendor.display.config@2.0
 
 # Display Device Config
 PRODUCT_COPY_FILES += \
