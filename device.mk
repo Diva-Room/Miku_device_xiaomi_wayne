f244a6ee27bdfec87c949e9736d023f279fce8d1
xiaoleGun <1592501605@qq.com>
2022-10-01 11:02:01 +0800

    wayne: Update DRM from S62Pro
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -199,7 +199,7 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.1.vendor
+    android.hardware.drm@1.3.vendor
 
 # fwk-detect
 PRODUCT_PACKAGES += \
