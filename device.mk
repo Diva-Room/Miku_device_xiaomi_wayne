ef292f208ccf35dd82d0b400318dd1ef9a9dc816
AmeChanRain <downloadbot007@gmail.com>
2023-10-05 12:26:45 +0000

    wayne: Ship with prebuilt libprotobuf-cpp
    * libprotobuf-cpp-full-vendorcompat and libprotobuf-cpp-lite-vendorcompat are deprecated
    
    Change-Id: Ic20975df9a2623df34b4902f4c2a0d3bfb1c4fe5

--- device.mk
+++ device.mk
@@ -354,9 +354,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power-libperfmgr/etc/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Protobuf
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
