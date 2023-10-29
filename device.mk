11e0ed9d21ea88e2b9e9653c3757d2088a966a59
xiaoleGun <1592501605@qq.com>
2023-10-29 08:14:12 +0800

    wayne: Inherit vibrator-vendor-product from vendor/qcom/opensource/vibrator
    Because this can lead to errors:
    'aidl/android/hardware/vibrator/BnVibrator.h' file not found
    
    Reason:
    https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/vibrator/-/commit/9650bd8ce731adab1b440182db199be9fc39c39f
    
    Change-Id: Id033cebead20957e1e097aafa0ea7861a88963ad
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -415,11 +415,7 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # WiFi
 PRODUCT_PACKAGES += \
