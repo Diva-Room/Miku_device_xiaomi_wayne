4186f0ccecdfe031d5bdc41f41451b10c69f948e
xiaoleGun <1592501605@qq.com>
2025-07-05 19:36:48 +0800

    wayne: Update Speed profile paths
    * source path "frameworks/base/config/boot-image-profile.txt" does not exist
    
    * Based on https://android.googlesource.com/platform/frameworks/base/+/6431bd08c47b93bdaee339d270287cbe36982e8d
    
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -431,7 +431,7 @@ PRODUCT_SHIPPING_API_LEVEL := 27
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # USB
 PRODUCT_PACKAGES += \
