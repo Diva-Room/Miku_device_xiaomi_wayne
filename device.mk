ccc9381797b6526b3c279751a70a89cd0260e716
xiaoleGun <1592501605@qq.com>
2025-07-14 21:47:08 +0800

    wayne: Switch thermal to AIDL
    Change-Id: Idb06cfefb79d7c52a662f567a1a3329f23fc2ce9
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- device.mk
+++ device.mk
@@ -344,7 +344,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal-service.qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
