25495c73075919326cbecd78cc2cfaf21a746174
xiaoleGun <1592501605@qq.com>
2025-07-15 12:34:59 +0800

    wayne: Patch lib-imsvideocodec
    Change-Id: I6cffbe30c7c00cd03f10b31ad4d38133c99f9f6a
    Signed-off-by: xiaoleGun <1592501605@qq.com>

--- extract-files.py
+++ extract-files.py
@@ -72,7 +72,7 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
-    'system_ext/lib64/lib-imscamera.so': blob_fixup()
+    ('system_ext/lib64/lib-imscamera.so', 'system_ext/lib64/lib-imsvideocodec.so'): blob_fixup()
         .add_needed('libgui_shim.so'),
     'vendor/bin/mlipayd@1.1': blob_fixup()
         .remove_needed('vendor.xiaomi.hardware.mtdservice@1.0.so'),
